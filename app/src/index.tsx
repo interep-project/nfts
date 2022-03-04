00cfbe7866caed009ea884aed12311cefd1b7be3
cedoor <me@cedoor.dev>
2022-03-04 18:20:25 +0100

    chore: update @interep/proof pkg dependency

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,3 @@
-import { keccak256 } from "@ethersproject/solidity"
 import createIdentity from "@interep/identity"
 import createProof from "@interep/proof"
 import detectEthereumProvider from "@metamask/detect-provider"
@@ -10,6 +9,10 @@ import {
     IconButton,
     InputLabel,
     Link,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
     MenuItem,
     Paper,
     Select,
@@ -20,12 +23,13 @@ import {
     Theme,
     Typography
 } from "@mui/material"
+import { LoadingButton } from "@mui/lab"
 import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { createStyles, makeStyles } from "@mui/styles"
-import { Contract, ethers } from "ethers"
+import { Contract, ethers, utils } from "ethers"
 import React from "react"
 import ReactDOM from "react-dom"
-import { abi } from "../static/InterepNFT.json"
+import { abi as contractAbi } from "../static/InterepNFT.json"
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -69,12 +73,15 @@ const theme = createTheme({
     }
 })
 
+const contractAddress = "0x4100e04576439D35E8933fF42ccAcC6C84b855bd"
+
 function App() {
     const classes = useStyles()
     const [_ethereumProvider, setEthereumProvider] = React.useState<any>()
     const [_tokenId, setTokenId] = React.useState<string>("")
     const [_oAuthProvider, setOAuthProvider] = React.useState<string>("")
-    const [_errorMessage, setErrorMessage] = React.useState<string>("")
+    const [_error, setError] = React.useState<boolean>(false)
+    const [_loading, setLoading] = React.useState<boolean>(false)
     const [_activeStep, setActiveStep] = React.useState<number>(0)
 
     React.useEffect(() => {
@@ -106,7 +113,7 @@ function App() {
 
     function handleNext() {
         setActiveStep((prevActiveStep: number) => prevActiveStep + 1)
-        setErrorMessage("")
+        setError(false)
     }
 
     function resetSteps() {
@@ -134,28 +141,31 @@ function App() {
             provider: _oAuthProvider.toLowerCase(),
             name: "gold"
         }
-        const externalNullifier = keccak256(["string", "string"], [groupId.provider, groupId.name])
+        const externalNullifier = utils.solidityKeccak256(["string", "string"], [groupId.provider, groupId.name])
         const signal = "nft"
 
         const zkFiles = { wasmFilePath: "./semaphore.wasm", zkeyFilePath: "./semaphore_final.zkey" }
 
+        setLoading(true)
+
         try {
             const proof = await createProof(identity, groupId, externalNullifier, signal, zkFiles)
-            const contract = new Contract("0xF9AB5f33A05fb51645bF8848b607f3E7341Fa6Bc", abi)
-
-            await contract.connect(signer).mint(proof[0], proof[2], proof[4])
+            const contract = new Contract(contractAddress, contractAbi)
 
             setTokenId(proof[2])
+
+            await contract.connect(signer).mint(proof[0], utils.formatBytes32String(signal), proof[2], proof[4])
+
             setActiveStep(3)
         } catch (error) {
             console.error(error)
 
-            setErrorMessage(
-                "Sorry, there was an error in the creation of your Semaphore proof. Please verify that you are in a 'gold' group."
-            )
+            setError(true)
 
             resetSteps()
         }
+
+        setLoading(false)
     }
 
     return (
@@ -167,11 +177,11 @@ function App() {
                     </Typography>
 
                     <Typography variant="body1" sx={{ mb: 4 }}>
-                        Join an Interep 'gold' group on&nbsp;
+                        Join an Interep gold group on&nbsp;
                         <Link href="https://kovan.interep.link" underline="hover" rel="noreferrer" target="_blank">
                             kovan.interep.link
                         </Link>
-                        &nbsp;and mint your NFT.
+                        , wait 1 minute and mint your NFT.
                     </Typography>
 
                     <Stepper activeStep={_activeStep} orientation="vertical">
@@ -208,16 +218,22 @@ function App() {
                             </StepContent>
                         </Step>
                         <Step>
-                            <StepLabel error={!!_errorMessage}>Mint your Interep NFT</StepLabel>
+                            <StepLabel error={!!_error}>Mint your Interep NFT</StepLabel>
                             <StepContent style={{ width: 400 }}>
-                                <Button fullWidth onClick={() => mintNFT()} variant="outlined">
+                                <LoadingButton
+                                    loading={_loading}
+                                    loadingIndicator="Loading..."
+                                    fullWidth
+                                    onClick={() => mintNFT()}
+                                    variant="outlined"
+                                >
                                     Mint NFT
-                                </Button>
+                                </LoadingButton>
                             </StepContent>
                         </Step>
                     </Stepper>
 
-                    {_tokenId && (
+                    {_activeStep === 3 && (
                         <Paper className={classes.results} sx={{ p: 3 }}>
                             <IconButton
                                 onClick={() => resetSteps()}
@@ -229,7 +245,7 @@ function App() {
                             <Typography variant="body1">
                                 You have minted your Interep NFT successfully:&nbsp;
                                 <Link
-                                    href={"https://kovan.etherscan.io/token/" + _tokenId}
+                                    href={"https://kovan.etherscan.io/token/" + contractAddress + "/?a=" + _tokenId}
                                     underline="hover"
                                     rel="noreferrer"
                                     target="_blank"
@@ -240,9 +256,27 @@ function App() {
                         </Paper>
                     )}
 
-                    {_errorMessage && (
+                    {_error && (
                         <Paper className={classes.results} sx={{ p: 3 }}>
-                            <Typography variant="body1">{_errorMessage}</Typography>
+                            <Typography variant="body1">
+                                Sorry, there was an error in the creation of your Semaphore proof.
+                            </Typography>
+                            <List sx={{ mb: 0 }}>
+                                <ListItem>
+                                    <ListItemText secondary="• Make sure you have enough balance in your wallet." />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText secondary="• Verify that you are part of a gold group." />
+                                </ListItem>
+                                <ListItemButton
+                                    component="a"
+                                    href={"https://kovan.etherscan.io/token/" + contractAddress + "/?a=" + _tokenId}
+                                    target="_blank"
+                                >
+                                    <ListItemText secondary="• Click here to check if you have already minted your NFT." />
+                                </ListItemButton>
+                            </List>
+                            <Typography variant="body1">{_error}</Typography>
                         </Paper>
                     )}
                 </Box>
